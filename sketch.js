7ead1dc96d2eed90a6fe72e6d2b026afd44f44a7
MatildeMammana <matilde.mammana@gmail.com>
2018-09-25 10:54:28 +0200

    Robot completed

--- sketch.js
+++ sketch.js
@@ -1,23 +1,79 @@
 function setup() {
 	// define artboard size
-	createCanvas(500,500);
+	createCanvas(500,600);
 	// Set here the values that are not changing all over the sketch
-	strokeWeight(2);
-	stroke(170);
+
 }
 
 function draw() {
 	background(204);
 
 	//Neck
-	line(250, 285, 250, 345);
+	noStroke();
+	fill(20,60,173); //Blue
+	rect(240, 285, 20, 40);
 
-	//Wheel
-	ellipse(250, 465, 70, 70);
+	strokeWeight(2);
+	stroke(0);
+
+	//Hair
+	line(250, 225, 270, 180);
+	line(250, 225, 230, 190);
+
+	//Arms
+	line(165, 330, 120, 420);
+	line(335, 330, 380, 420);
+
+	noStroke();
+
+	//Shoulders
+	fill(73,168,206);
+	ellipse(165, 335, 20);
+	fill(73,168,206);
+	ellipse(335, 335, 20);
+
+	//Feet
+	fill(73,168,206);
+	ellipse(230, 510, 20);
+	fill(73,168,206);
+	ellipse(270, 510, 20);
+
+	//Legs
+	fill(20,60,173); //Blue
+	rect(220, 480, 20, 30);
+	fill(20,60,173); //Blue
+	rect(260, 480, 20, 30);
 
 	//Body
-	rect(200, 325, 100, 140);
+	fill(73,168,206);
+	rect(165, 325, 170, 20);
+	fill(20,60,173); //Blue
+	arc(250, 345, 170, 300 , 0, 3.14);
+	fill(0, 140, 173, 75.5); //Lightblue
+	arc(250, 345, 130, 200 , 0, 3.14);
+
+
+	//Ears
+	fill(73,168,206);
+	ellipse(165 ,265, 50);
+	fill(73,168,206);
+	ellipse(335, 265, 50);
 
 	//Head
-	ellipse(250, 240, 90, 90);
+	ellipse(250, 240, 90, 50);
+	fill(20,60,173); //Blue
+	rect(165, 240, 170, 50);
+	fill(0, 140, 173, 75.5); //Lightblue
+	ellipse(250, 265, 160, 40);
+
+
+	//Eyes
+	fill(255, 255, 255);
+	ellipse(230, 265, 12.5);
+	fill(255, 255, 255);
+	ellipse(270, 265, 20);
+	fill(0,0,0);
+	ellipse(230,265, 5);
+	ellipse(270,265,5);
+
 }
